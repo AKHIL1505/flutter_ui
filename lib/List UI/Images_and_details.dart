2617d65d641ab643ac48eca3420e0e6526971667
AKHIL <akhil.1505.ak@gmail.com>
2023-03-27 17:43:52 +0530

    Initial commit

---
+++ lib/List UI/Images_and_details.dart
@@ -0,0 +1,16 @@
+var citesName = ["Delhi", "Finland", "London", "Vancouver", "New York"];
+var country = ["India", "Europe", "UK", "Canada", "USA"];
+var population = [
+  "Population : 32.9 mill",
+  "Population: 5.54 mill",
+  "Population: 8.8 mill",
+  "Population: 2.6 mill",
+  "Population: 19.68 mill"
+];
+var image = [
+  "assets/Images/delhi.jpg",
+  "assets/Images/Finland.jpg",
+  "assets/Images/London.jpg",
+  "assets/Images/Vancouver.jpg",
+  "assets/Images/New York.jpg"
+];
---
+++ lib/List UI/listUI.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'Images_and_details.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CitesAroundWorld(),
+    debugShowCheckedModeBanner: false,theme: ThemeData(primarySwatch: Colors.deepOrange),
+  ));
+}
+
+class CitesAroundWorld extends StatelessWidget {
+  const CitesAroundWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(80),
+            child: AppBar(
+              titleTextStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+              centerTitle: true,
+              title: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text("Cities Around World"),
+              ),
+            )),
+        body: ListView.builder(
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Card(color: Colors.orange,
+                  child: Row(children: [
+                    Container(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Image(
+                              fit: BoxFit.fill,
+                              height: 130,
+                              width: 200,
+                              image: AssetImage(image[index])),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: Wrap(direction: Axis.vertical, children: [
+                        Text(
+                          citesName[index],
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Text(country[index],style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500)),
+                        ),
+                        Text(population[index]),
+                      ]),
+                    )
+                  ]),
+                ),
+              );
+            },
+            itemCount: citesName.length));
+  }
+}
