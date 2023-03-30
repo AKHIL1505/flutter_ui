5fbf980fc4dec3397f35b9aea606bd68dcc9b397
AKHIL <akhil.1505.ak@gmail.com>
2023-03-30 16:09:01 +0530

    COMMIT

--- lib/Grid View With Card/GridWithCard.dart
+++ lib/Grid View With Card/GridWithCard.dart
@@ -1,218 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'gridWithCard_Details.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: const GridViewWithCard(),
+      home: GridViewWithCard(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           cardColor: Colors.green,
           cardTheme: CardTheme(
             shape: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.transparent),
+                // borderSide: const BorderSide(color: Colors.transparent),
                 borderRadius: BorderRadius.circular(30)),
           ),
-          textTheme: const TextTheme(
-              bodyMedium: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          )),
-          iconTheme: const IconThemeData(size: 50, color: Colors.black26)),
+          iconTheme: const IconThemeData(size: 40, color: Colors.black26)),
     ),
   );
 }
 
 class GridViewWithCard extends StatelessWidget {
-  const GridViewWithCard({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white24,
-        body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 30, right: 10, left: 10),
-        child: StaggeredGrid.count(
-          crossAxisCount: 10,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 5,
-          children: [
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.purpleAccent,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.home), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("HOME"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.orange,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.mail), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("MAIL"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.lightGreenAccent,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.phone), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("PHONE"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.greenAccent,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.camera_alt_outlined),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text("CAMERA"),
-                        )
-                      ]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.limeAccent,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.download), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("DOWNLOAD"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.white60,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.account_circle), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("ACCOUNT"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.lightBlueAccent,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.image), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("PICTURE"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.pink,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.delete), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("TRASH"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.indigo,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.send),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text("SEND"),
-                        )
-                      ]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.deepOrange,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.search), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("SEARCH"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.teal,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.video_call), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("VIDEO"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.green,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.mic_off), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("MUTE"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.purple,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.headphones), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("HEADSET"),
-                      )]),
-                )),
-            StaggeredGridTile.count(
-                crossAxisCellCount: 5,
-                mainAxisCellCount: 3,
-                child: Card(
-                  color: Colors.lightBlueAccent,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [Icon(Icons.volume_down), Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("VOLUME"),
-                      )]),
-                )),
-          ],
-        ),
+    return Scaffold(
+        //   backgroundColor: Colors.white24,
+        body: Padding(
+      padding: const EdgeInsets.only(top: 30, right: 5, left: 5),
+      child: GridView.builder(
+        itemCount: gridData.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 2, mainAxisSpacing: 9, crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 30,
+            shadowColor: Colors.yellow,
+            color: cardColors[index],
+            child: Align(
+              alignment: Alignment.center,
+              child: ListTile(
+                leading: gridIcons[index],
+                title: Text(
+                  gridData[index],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     ));
   }
---
+++ lib/Grid View With Card/gridWithCard_Details.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+var gridData = [
+  "HOME",
+  "MAIL",
+  "PHONE",
+  "CAMERA",
+  "BACKUP",
+  "ACCOUNT",
+  "PICTURE",
+  "TRASH",
+  "SEND",
+  "SEARCH",
+  "VIDEO",
+  "MUTE",
+  "HEADSET",
+  "VOLUME"
+];
+var gridIcons = [
+  Icon(Icons.home),
+  Icon(Icons.mail),
+  Icon(Icons.phone),
+  Icon(Icons.camera_alt_outlined),
+  Icon(Icons.backup_outlined),
+  Icon(Icons.account_circle),
+  Icon(Icons.image),
+  Icon(Icons.delete),
+  Icon(Icons.send),
+  Icon(Icons.search),
+  Icon(Icons.video_call),
+  Icon(Icons.mic_off),
+  Icon(Icons.headphones),
+  Icon(Icons.volume_down),
+];
+var cardColors = [
+  Colors.purpleAccent,
+  Colors.orange,
+  Colors.lightGreenAccent,
+  Colors.greenAccent,
+  Colors.limeAccent,
+  Colors.white60,
+  Colors.lightBlueAccent,
+  Colors.pink,
+  Colors.indigo,
+  Colors.deepOrange,
+  Colors.teal,
+  Colors.green,
+  Colors.purple,
+  Colors.lightBlueAccent,
+];
